e1c59f235e8ec82599e1682320e90debbb3fc8c9
thanhlong310520 <109069949+thanhlong310520@users.noreply.github.com>
2023-04-01 14:48:31 +0700

    Add files via upload
    source code 2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bai3_HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello: " + name);
+        }
+    }
+}
