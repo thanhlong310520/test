887610b106d03c20c3339648a092a59abf69f2c9
TLong <tilthanhlong@gmail.com>
2023-04-06 14:24:19 +0700

    test2

---
+++ Bai1_HienThiThoiGian/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bai1_HienThiThoiGian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+        }
+    }
+}
